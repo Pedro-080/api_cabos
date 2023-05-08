06ee1472fa6007e8e7511091b02479ac7302945e
Pedro-080 <pa.nascimento12@gmail.com>
2023-05-08 13:01:00 -0300

    08.05.23 13:01hrs

--- app.py
+++ app.py
@@ -44,9 +44,12 @@ def set_cabo(index):
     return condutor.cabo
 
 @app.get('/set_vao') #/set_vao?vao=50.20
-def set_vao():
+def set_vao(vao=0):
     #define a variavel global "vao" como entrada
-    app.config['vao'] = request.args.get('vao', default=0.0, type=float)
+    if vao == 0:
+        app.config['vao'] = request.args.get('vao', default=0.0, type=float)
+    else:
+        app.config['vao'] = vao
     
     # variavel de teste
     meu_vao = app.config['vao']
@@ -57,41 +60,60 @@ def set_vao():
 @app.get('/get_vao') #funcao de teste que retorna o valor global de "vao"
 def get_vao():
     vao_global = app.config['vao']
-    return f'Vao global: {vao_global}'
+    return vao_global
 
 @app.get('/set_t01') #/set_t01?t01=230
-def set_t01():
-    #define a variavel global "vao" como entrada
-    app.config['t01'] = request.args.get('t01', default=0.0, type=float)
+def set_t01(t01=0):
+      #define a variavel global "t01" como entrada
+    if t01 == 0:
+        app.config['t01'] = request.args.get('t01', default=0.0, type=float)
+    else:
+        app.config['t01'] = t01  
     
     # variavel de teste
     t01 = app.config['t01']
     
     # retorna a variavel de teste meu_vao
-    return f'T01: {t01}'
+    return t01
 
 @app.get('/get_t01') #funcao de teste que retorna o valor global de "t01"
 def get_t01():
     t01_global = app.config['t01']
-    return f'Vao global: {t01_global}'
+    return t01_global
 
 @app.get('/set_tmin') #/set_tmin?tmin=50.20
-def set_tmin():
-    app.config['t_min'] = request.args.get('tmin', default=0.0, type=float)
+def set_tmin(tmin=0):
+    #define a variavel global "tmin" como entrada
+    if tmin == 0:
+        app.config['t_min'] = request.args.get('tmin', default=app.config['t_min'], type=float)
+    else:
+        app.config['t_min'] = tmin   
+    
+    # app.config['t_min'] = request.args.get('tmin', default=0.0, type=float)
     t_min = app.config['t_min']
-    return f'Temperatura mínima: {t_min} °C'
+    return t_min
     
 @app.get('/set_teds') #/set_teds?teds=50.20
-def set_teds():
-    app.config['t_eds'] = request.args.get('teds', default=0.0, type=float)  
+def set_teds(teds=0):
+    #define a variavel global "teds" como entrada
+    if teds == 0:
+        app.config['t_eds'] = request.args.get('teds', default=app.config['t_eds'], type=float)
+    else:
+        app.config['t_eds'] = teds      
+    
     t_eds = app.config['t_eds']
-    return f'Temperatura eds: {t_eds} °C'
+    return t_eds
 
 @app.get('/set_tope') #/set_tope?tope=75
-def set_tope():
-    app.config['t_ope'] = request.args.get('tope', default=0.0, type=float)
+def set_tope(tope=0):
+    #define a variavel global "tope" como entrada
+    if tope == 0:
+        app.config['t_ope'] = request.args.get('tope', default=app.config['t_ope'], type=float)
+    else:
+        app.config['t_ope'] = tope  
+    
     t_ope = app.config['t_ope']
-    return f'Temperatura de operacao: {t_ope} °C'
+    return t_ope
 
 @app.get('/load_temp') #carrega todas as temperaturas
 def load_temp():
@@ -101,12 +123,33 @@ def load_temp():
     condutor.set_temperaturas(t_min,t_eds,t_ope)
     return condutor.get_temperaturas()
 
+@app.get('/cabo/<int:index>/load') #/cabo/2/load?vao=100&t01=200&tmin=16&teds=22&tope=75
+def load(index):
+    set_cabo(index)
+    set_vao(request.args.get('vao',default=0.0, type=float))
+    set_t01(request.args.get('t01',default=0.0, type=float))
+    set_tmin(request.args.get('tmin',default=app.config['t_min'],type=float))
+    set_teds(request.args.get('teds',default=app.config['t_eds'],type=float))
+    set_tope(request.args.get('tope',default=app.config['t_ope'],type=float))
+    
+    load_temp()
+    vao = get_vao()
+    t01 = get_t01() 
+    
+    condutor.dados_esticamento(t01,vao)
+    esticamento = condutor.esticamento.to_json(orient='records')  
+
+    return esticamento
+
+
+
 @app.get('/cabo/esticamento')
 def esticamento():
     load_temp()
     vao=app.config['vao']
     t01=app.config['t01']
-    condutor.dados_esticamento(vao,t01)
+    print(t01)
+    condutor.dados_esticamento(t01,vao)
     esticamento = condutor.esticamento.to_json(orient='records')
  
     # var_class = condutor.__dict__
